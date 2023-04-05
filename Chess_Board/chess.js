8d19b78b4eb6a8ccb23f3660182fd21990546799
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-05 22:58:50 +0545

    Create chess.js

---
+++ Chess_Board/chess.js
@@ -0,0 +1,10 @@
+var i, j;
+document.write("<table border='1'")
+for (i=1; i<=8; i++){
+    document.write("<tr>")
+    for (j=1; j<=8; j++){
+     document.write("<td></td>")
+    }
+    document.write("</tr>")
+}
+document.write("</table>");
