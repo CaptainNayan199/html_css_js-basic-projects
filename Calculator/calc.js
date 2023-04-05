d05f5dca389b5fe07083e00bd057fb0bd9ffe451
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-05 22:56:35 +0545

    Update calc.js

--- Calculator/calc.js
+++ Calculator/calc.js
@@ -1,1 +1,30 @@
 
+
+function num(e) {
+    var textbox = document.getElementById("textbox");
+    var num
+    num = textbox.value + e
+    textbox.value = num
+}
+
+
+function calculate() {
+        var num1 = document.getElementById("textbox").value
+        let result = eval(num1)
+        textbox.value = 0
+        textbox.value = result 
+}
+
+function ac(){
+    textbox.value = 0
+}
+
+function off(){
+    textbox.value = " "
+}
+function del(){
+    let str = document.getElementById("textbox").value
+    str = str.slice(0, -1)
+    textbox.value = str
+
+}
